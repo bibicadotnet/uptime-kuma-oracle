73d368209958e62eead9e9c7c6cc81f3bd9b67c6
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-18 21:26:44 +0700

    Create uptime-kuma-oracle.sh

---
+++ uptime-kuma-oracle.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
+sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $(whoami)
+sudo systemctl start docker
+sudo systemctl enable docker
+apt install docker-compose -y
+docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
